5b9b86991f422688228f0046b3d2d1993e6e6b0f
Mathias Ertl <mati@er.tl>
2015-11-18 21:24:10 +0100

    improve setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,31 @@
 
 from distutils.core import setup
 
-setup(name='xmpp-backends',
-      version='0.1',
-      description='A set of classes with common interfaces to communicate with XMPP servers.',
-      author='Mathias Ertl',
-      author_email='mati@jabber.at',
-      url='https://github.com/mathiasertl/xmpp-backends',
-      packages=['xmpp_backends'],
+setup(
+    name='xmpp-backends',
+    version='0.1',
+    description='A set of classes with common interfaces to communicate with XMPP servers.',
+    author='Mathias Ertl',
+    author_email='mati@jabber.at',
+    url='https://github.com/mathiasertl/xmpp-backends',
+    packages=['xmpp_backends'],
+    license="GNU General Public License (GPL) v3",
+    install_requires=[
+        'six>=1.10.0'
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Communications :: File Sharing",
+    ]
 )
