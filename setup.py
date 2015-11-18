046dc679306b581d56b3028cdd90e22eb2ad31e5
Mathias Ertl <mati@er.tl>
2015-11-18 21:20:49 +0100

    version version of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='xmpp-backends',
+      version='0.1',
+      description='A set of classes with common interfaces to communicate with XMPP servers.',
+      author='Mathias Ertl',
+      author_email='mati@jabber.at',
+      url='https://github.com/mathiasertl/xmpp-backends',
+      packages=['xmpp_backends'],
+)
