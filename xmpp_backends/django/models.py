c6d281206598dc583e174ea0d31bf0c3ef7f0674
Mathias Ertl <mati@er.tl>
2016-12-04 15:38:36 +0100

    use new file header

--- xmpp_backends/django/models.py
+++ xmpp_backends/django/models.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of django-xmpp-register (https://account.jabber.at/doc).
+# This file is part of xmpp-backends (https://github.com/mathiasertl/xmpp-backends).
 #
-# django-xmpp-register is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# xmpp-backends is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# django-xmpp-register is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
+# xmpp-backends is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with
-# django-xmpp-register.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with xmpp-backends.  If
+# not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
 
 from . import backend
 from django.contrib.auth.models import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
-
 
 
 class XmppBackendUser(AbstractBaseUser):
@@ -54,4 +51,3 @@ class XmppBackendUser(AbstractBaseUser):
     @property
     def domain(self):
         return self.get_username().split('@', 1)[1]
-
