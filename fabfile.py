0d4d3502b4cb033a4932aec1bf900e183b750690
Mathias Ertl <mati@er.tl>
2017-08-05 11:22:46 +0200

    start rewriting test script as fab task

--- fabfile.py
+++ fabfile.py
@@ -16,6 +16,11 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import importlib
+import json
+import os
+import sys
+
 from fabric.api import local
 from fabric.api import task
 
@@ -26,3 +31,20 @@ def check():
 
     local('isort --check-only --diff -rc xmpp_backends setup.py fabfile.py')
     local('flake8 xmpp_backends/ setup.py fabfile.py')
+
+
+@task
+def test_backend(backend, config_path=''):
+    sys.path.insert(0, '.')
+    mod_path, cls_name = backend.rsplit('.', 1)
+    importlib.import_module('xmpp_backends')
+    mod = importlib.import_module(mod_path)
+    cls = getattr(mod, cls_name)
+
+    config_path = config_path or os.path.join('config', '%s.json' % mod.__name__.rsplit('.', 1)[-1])
+    with open(config_path) as stream:
+        config = json.load(stream)
+
+    backend = cls(**config.get('kwargs', {}))
+
+    backend.user_exists('foo', 'bar')
