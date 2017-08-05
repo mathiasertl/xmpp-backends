eae0d29a6e53f8e8d52171e7d17f889b4eddceac
Mathias Ertl <mati@er.tl>
2017-08-05 11:09:14 +0200

    configure test-suite

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     install_requires=[
         'six>=1.10.0',
     ],
+    test_suite='tests',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
