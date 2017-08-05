9178802757fd01bef99ca4bd9aae4ca70197b806
Mathias Ertl <mati@er.tl>
2017-08-05 12:20:01 +0200

    sort imports

--- fabfile.py
+++ fabfile.py
@@ -14,8 +14,8 @@
 # <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
-from __future__ import unicode_literals
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import importlib
 import json
