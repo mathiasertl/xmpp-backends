84154eb15dd6915a2ee5964730167a2f8c673bae
Mathias Ertl <mati@er.tl>
2017-08-08 19:22:01 +0200

    test more functions in backend

--- fabfile.py
+++ fabfile.py
@@ -139,10 +139,46 @@ def test_backend(backend, domain, config_path='', version=''):
         error('Did not get same last activity back: %s vs %s' % (now.isoformat(), last.isoformat()))
     ok()
 
+    print('Send user a message... ', end='')
+    # message the user - we can not do anything with the message without an XMPP connection, so we just
+    # execute it.
+    msg = backend.message_user(username1, domain, 'subject', 'message')
+    if msg is not None:
+        error('message_user() did not return None: %s' % msg)
+    ok()
+
+    print('Get (empty) list of user sessions... ', end='')
+    sessions = backend.user_sessions(username1, domain)
+    if sessions != []:
+        error('user_sessions() did not return empty list: %s' % sessions)
+    ok()
+
+    # block user
+    print('Block user and check previous passwords... ', end='')
+    blk = backend.block_user(username1, domain)
+    if blk is not None:
+        error('block_user() did not return None: %s' % blk)
+    if backend.check_password(username1, domain, password1) is not False:
+        error('False password is accepted.')
+    if backend.check_password(username1, domain, password2) is not False:
+        error('False password is accepted.')
+    ok()
+
     # remove user again
     print('Remove user... ', end='')
     if backend.remove_user(username1, domain) is not None:
         error('remove_user() did not return None.')
     if backend.all_users(domain) != initial_users:
         error('all_users() did not return an empty set after removing user.')
+    ok()
+
+    print('Test statistics... ', end='')
+    if backend.stats('registered_users') != 0:
+        error('registered_users did not return 0.')
+    if backend.stats('registered_users', domain) != 0:
+        error('registered_users for domain did not return 0.')
+    if backend.stats('online_users') != 0:
+        error('online_users did not return 0.')
+    if backend.stats('online_users', domain) != 0:
+        error('online_users for domain did not return 0.')
     ok()
