297bdbdd50473eb89ee4949475dc19d3cb5caae6
Mathias Ertl <mati@er.tl>
2017-09-17 17:28:33 +0200

    add constants for connections

---
+++ xmpp_backends/constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of xmpp-backends (https://github.com/mathiasertl/xmpp-backends).
+#
+# xmpp-backends is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# xmpp-backends is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with xmpp-backends.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+CONNECTION_XMPP = 0
+CONNECTION_HTTP_POLLING = 1
+CONNECTION_HTTP_BINDING = 2
+CONNECTION_WEBSOCKETS = 3
+CONNECTION_UNKNOWN = 4
