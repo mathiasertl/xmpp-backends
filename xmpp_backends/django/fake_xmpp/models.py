d6c3b122efc2e6fa19f9c2184367b9704fd668ea
Mathias Ertl <mati@er.tl>
2018-04-04 21:16:46 +0200

    start with actual backend

--- xmpp_backends/django/fake_xmpp/models.py
+++ xmpp_backends/django/fake_xmpp/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-#from __future__ import unicode_literals
 
-#from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils import timezone
+
+
+class FakeUser(AbstractBaseUser):
+    last_activity = models.DateTimeField(default=timezone.now)
