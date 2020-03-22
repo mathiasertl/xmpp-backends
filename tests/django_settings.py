225c81f8e323df6c789259b31dd091c9ffe74c1c
Mathias Ertl <mathias.ertl@tttech.com>
2020-03-22 12:17:34 +0100

    add required settings

--- tests/django_settings.py
+++ tests/django_settings.py
@@ -3,9 +3,9 @@
 # It's an error if the next version is already broken
 import warnings
 from django.utils import deprecation  # NOQA
-warnings.filterwarnings('error', category=deprecation.RemovedInNextVersionWarning)  # NOQA
-if hasattr(deprecation, 'RemovedInDjango30Warning'):
-    warnings.filterwarnings('always', category=deprecation.RemovedInDjango30Warning)  # NOQA
+#warnings.filterwarnings('error', category=deprecation.RemovedInNextVersionWarning)  # NOQA
+#if hasattr(deprecation, 'RemovedInDjango30Warning'):
+#    warnings.filterwarnings('always', category=deprecation.RemovedInDjango30Warning)  # NOQA
 
 DEBUG = True
 SECRET_KEY = 'dummy'
@@ -26,4 +26,30 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'xmpp_backends.django.fake_xmpp',
+]
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
